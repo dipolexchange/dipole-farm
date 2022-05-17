c2337fd07cb9ef07d105ffb0fd50b4c1a43c890c
fksyuan <fksyuan@163.com>
2022-05-17 17:40:36 +0800

    add config file

---
+++ .solcover.js
@@ -0,0 +1,13 @@
+// Contracts are compiled without optimization
+// and with gas estimation distortion
+// https://github.com/sc-forks/solidity-coverage/blob/master/HARDHAT_README.md#usage
+
+module.exports = {
+  skipFiles: [
+    "libs",
+    "libs",
+    "libs"
+  ],
+  measureStatementCoverage: false,
+  measureFunctionCoverage: true,
+};
